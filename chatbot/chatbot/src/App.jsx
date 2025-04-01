c4969f25eea0147a6957dab825037982332a385b
Angelica Adjei-Kwarteng <angelicaakwarteng24@gmail.com>
2025-04-01 14:56:20 +0000

    Chatbot files
    Converted chatbot to react file.

---
+++ chatbot/chatbot/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chatbox from './Chatbox';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Chatbox />
+    </div>
+  );
+}
+
+export default App;
